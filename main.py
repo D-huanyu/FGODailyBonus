2bc388506e319cabe09c0918dbae7d186225bb86
D-huanyu <55185517+D-huanyu@users.noreply.github.com>
2025-06-25 22:28:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
         fgourl.ReadConf()
         fgourl.gameData()
         print(f'待签到: {userNums}个')
-        res = '【登录信息】\n'
+               res = ' 【登录信息】\n' + '待签到: {userNums}个'
         for i in range(userNums):
             try:
                 instance = user(userIds[i], authKeys[i], secretKeys[i])
